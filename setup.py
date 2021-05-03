01fed5e9c3a25ec628992c44d4a488f7e36bd3e6
c2python-coding <c2python.coding@protonmail.com>
2021-05-03 15:42:52 -0500

    fixing setup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python 3
 from distutils.core import setup
+import setuptools
 
 setup(
     name='Jtool',
     version='0.1.0',
     author='c2python-coding',
     author_email='c2python.coding@protonmail.com',
-    packages=['jtool'],
+    packages=setuptools.find_packages(),
     url='https://github.com/c2python-coding/Jtool',
     description='Tool for working with json data',
     long_description=open('README.md').read(),
