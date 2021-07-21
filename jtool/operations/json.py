be0c6a9730658be5ff4c2decd99c18ced3e39edc
ghost <c2python.coding>
2021-07-20 23:35:20 -0500

    added text processing ops

--- jtool/operations/json.py
+++ jtool/operations/json.py
@@ -27,7 +27,7 @@ def SPLIT_OP():
     return lambda data: [{key: data[key]} for key in lambda_type(data, dict)]
 
 
-@register_command("remove")
-def REMOVE_OP(key):
+@register_command("delete")
+def DELETE_OP(key):
     '''removes a entry from json by key'''
     return lambda data: [{key: data[key]} for key in lambda_type(data, dict)]
--- jtool/operations/text.py
+++ jtool/operations/text.py
@@ -20,4 +20,32 @@ def SPLITLINES_OP(re_delimeter):
 def STRIP_OP():
     '''removes leading/trailing spaces'''
     striplambda = lambda data: lambda_type(data, str).strip()
-    return lambda instring: exception_wrapper(striplambda, instring, "error removing spaces")
+    return lambda instring: exception_wrapper(striplambda, instring, "error removing leading/trailing spaces")
+
+
+@register_command("prepend")
+def PREPEND_OP(string):
+    '''add text to beggining'''
+    textlambda = lambda data, txt=string: txt + lambda_type(data, str)
+    return lambda instring: exception_wrapper(textlambda, instring, "error prepending text")
+
+
+@register_command("append")
+def APPEND_OP(string):
+    '''add text to the end'''
+    textlambda = lambda data, txt=string:  lambda_type(data, str) + txt
+    return lambda instring: exception_wrapper(textlambda, instring, "error appending text")
+
+@register_command("remove")
+def REMOVE_OP(string):
+    '''removes text'''
+    textlambda = lambda data, txt=string:  lambda_type(data, str).replace(string,"")
+    return lambda instring: exception_wrapper(textlambda, instring, "error removing text")
+
+@register_command("replace")
+def REPLACE_OP(string):
+    '''replaces text, argument in the form of (searchtext,replacetext)
+    if comma character is desired to be searched or replaced, use %comma% in place'''
+    args = [x.replace("%comma%",",") for x in string.split(",")]
+    textlambda = lambda data, repspec=args:  lambda_type(data, str).replace(repspec[0],repspec[1])
+    return lambda instring: exception_wrapper(textlambda, instring, "error replacing text")
